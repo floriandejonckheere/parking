cc832969e8bd84618482fd18ea30aa6d45e4ac27
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-02-27 14:43:09 +0100

    Add simple, step-based algorithm

--- lib/parking/algorithms/simple.rb
+++ lib/parking/algorithms/simple.rb
@@ -3,8 +3,36 @@
 module Parking
   module Algorithms
     class Simple
+      ACTIONS = [
+        *35.times.map { { accelerate: true } },
+        *15.times.map { { brake: true } },
+        *38.times.map { { reverse: true, right: true } },
+        *13.times.map { { reverse: true } },
+        *30.times.map { { reverse: true, left: true } },
+        *5.times.map { { left: true } },
+        *4.times.map { { accelerate: true, right: true } },
+        *30.times.map { { accelerate: true } },
+        *10.times.map { { brake: true } },
+      ].freeze
+
+      attr_reader :cursor
+
+      def initialize
+        @cursor = 0
+      end
+
       def run(car)
-        puts "Position (#{car.score.truncate(2)}) - damage (#{car.damage.truncate(2)}) = score (#{(car.score - car.damage).truncate(2)})"
+        puts action.keys.join(", ")
+
+        car.drive(**action)
+
+        @cursor += 1
+      end
+
+      private
+
+      def action
+        ACTIONS[cursor] || {}
       end
     end
   end
